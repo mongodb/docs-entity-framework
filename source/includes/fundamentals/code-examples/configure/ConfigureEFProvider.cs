8a185ee488e0acf7a90e10f1376795833de939cd
Jordan Smith <jordan.smith@mongodb.com>
2024-01-05 15:07:09 -0800

    first draft configuration page

---
+++ source/includes/fundamentals/code-examples/configure/ConfigureEFProvider.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+// start-use-mongodb
+var mongoClient = new MongoClient("<Your MongoDB Connection URI>");
+var mongoDatabase = mongoClient.GetDatabase("<Database Name>");
+
+var dbContextOptions =
+    new DbContextOptionsBuilder<MyDbContext>().UseMongoDB(mongoClient,
+        mongoDatabase.DatabaseNamespace.DatabaseName);
+
+var db = new MyDbContext(dbContextOptions.Options);
+// end-use-mongodb
+
+db.Customers.Add(new Customer() { name = "John Doe", Order = "1 Green Tea" });
+db.SaveChanges();
+
+// start-customer
+public class Customer
+{
+    public ObjectId Id { get; set; }
+    public String Name { get; set; }
+    public String Order { get; set; }
+}
+// end-customer
+
+// start-db-context
+internal class MyDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; init; }
+
+    public MyDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Customer>().ToCollection("customers");
+    }
+}
+// end-db-context
