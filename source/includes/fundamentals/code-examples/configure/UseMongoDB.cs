46a04109825a53df25d0871afd03f3129d92e4ac
Jordan Smith <jordan.smith@mongodb.com>
2024-01-08 10:30:17 -0800

    delete unused files

--- source/includes/fundamentals/code-examples/configure/UseMongoDB.cs
+++
@@ -1,8 +0,0 @@
-var mongoClient = new MongoClient("<Your MongoDB Connection URI>");
-var mongoDatabase = mongoClient.GetDatabase("<Database Name>");
-
-var dbContextOptions =
-    new DbContextOptionsBuilder<MyDbContext>().UseMongoDB(mongoClient,
-        mongoDatabase.DatabaseNamespace.DatabaseName);
-
-var db = new MyDbContext(dbContextOptions.Options);
